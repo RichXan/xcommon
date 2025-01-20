663b367c9f773cf7a9474f7d2233c336002e88ee
xan <rich4xan@gmail.com>
2025-01-20 11:09:14 +0800

    refactor(xhttp): simplify PageReq structure by removing OrderBy and GetOrderBy methods; default order is now set to "id desc"

--- xhttp/request.go
+++ xhttp/request.go
@@ -1,12 +1,9 @@
 package xhttp
 
-import "fmt"
-
 type PageReq struct {
 	Current int    `json:"current" form:"current" binding:"omitempty,min=1" default:"1"` // 当前页
 	Size    int    `json:"size" form:"size" binding:"omitempty,min=1" default:"10"`      // 每页大小
 	Order   string `json:"order" form:"order" binding:"omitempty" default:"id"`          // 排序字段
-	OrderBy string `json:"order_by" form:"order_by" binding:"omitempty" default:"desc"`  // 排序方式
 }
 
 func (p PageReq) GetCurrent() int {
@@ -25,16 +22,9 @@ func (p PageReq) GetSize() int {
 
 func (p PageReq) GetOrder() string {
 	if p.Order == "" {
-		return "id"
+		return "id desc"
 	}
 	return p.Order
-}
-
-func (p PageReq) GetOrderBy() string {
-	if p.OrderBy == "" {
-		return "desc"
-	}
-	return p.OrderBy
 }
 
 func (p PageReq) GetOffset() int {
@@ -46,8 +36,4 @@ func (p PageReq) GetOffset() int {
 
 func (p PageReq) GetLimit() int {
 	return p.GetSize()
-}
-
-func (p PageReq) GetOrderString() string {
-	return fmt.Sprintf("%s %s", p.GetOrder(), p.GetOrderBy())
 }
