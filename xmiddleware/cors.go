a95b6c2365c8b7ff019769f1e21aa4fb7e27f9df
xan <rich4xan@gmail.com>
2025-03-15 00:40:12 +0800

    feat(xmiddleware): enhance CORS middleware to support dynamic origin and improved header management

--- xmiddleware/cors.go
+++ xmiddleware/cors.go
@@ -1,22 +1,27 @@
 package xmiddleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // Cors 跨域中间件
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
 		}
-
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
 		c.Next()
 	}
-} 
+}
